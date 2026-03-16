ba1b2b8157c4e871f8cf93fbdd75b521e03c875c
Corentin v <17.corentinv@gmail.com>
2026-03-16 16:11:14 +0100

    Ajout du fichier .jar, création de l'administrateur en BDD

--- src/model/Utilisateur.java
+++ src/model/Utilisateur.java
@@ -8,6 +8,13 @@ public abstract class Utilisateur {
 	private String identifiantConnexion;
 	private String motDePasse;
 	
+	public Utilisateur(String nom, String prenom, String identifiantConnexion, String motDePasse) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.identifiantConnexion = identifiantConnexion;
+		this.motDePasse = motDePasse;
+	}
+	
 	public Utilisateur(int id, String nom, String prenom, String identifiantConnexion, String motDePasse) {
 		this.id = id;
 		this.nom = nom;
@@ -15,6 +22,34 @@ public abstract class Utilisateur {
 		this.identifiantConnexion = identifiantConnexion;
 		this.motDePasse = motDePasse;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public String getIdentifiantConnexion() {
+		return identifiantConnexion;
+	}
+
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+	@Override
+	public String toString() {
+		return "Utilisateur [id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", identifiantConnexion="
+				+ identifiantConnexion + ", motDePasse=" + motDePasse + "]";
+	}
+	
+	
 	
 	
 	
